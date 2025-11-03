89ea7b2eff762c0df60d5e599c0e9aeca5fb1ab6
RyanDonze <ryan.donze@ceff.ch>
2025-11-03 13:45:10 +0100

    Modification du app

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:portes_ouvertes/src/features/room/presentation/room_list/room_list_screen.dart';
-import 'package:portes_ouvertes/src/features/user/presentation/user_creation/user_creation.dart';
 import 'package:portes_ouvertes/src/theme/theme.dart';
 
 class MyApp extends StatelessWidget {
@@ -11,8 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Portes ouvertes',
       theme: blackTheme,
-      // home: const RoomListScreen(),
-      home: const UserCreation(),
+      home: const RoomListScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
